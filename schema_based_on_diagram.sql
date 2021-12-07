e543727ea70028fd04e04ed823b1fea5c2bb5716
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-12-07 22:14:19 +0300

    Delete extra treatments table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,13 +12,6 @@ CREATE TABLE treatments(
     PRIMARY KEY(id)
 );
 
-CREATE TABLE treatments(
-    id  INT GENERATED ALWAYS AS IDENTITY,
-    type VARCHAR(255),
-    name VARCHAR(255),
-    PRIMARY KEY(id)
-);
-
 
 CREATE TABLE medical_histories_treatments(
     id  INT GENERATED ALWAYS AS IDENTITY,
